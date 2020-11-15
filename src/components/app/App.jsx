754e781616d845991f64e4f4577e6d95bb9d72c4
shaikmalikbasha <shaikmalikbasha1998@gmail.com>
2020-11-15 13:16:36 +0530

    Basic layout completed.

---
+++ src/components/app/App.jsx
@@ -0,0 +1,15 @@
+import Header from '../header/Header';
+import Footer from '../footer/Footer';
+import Container from '../container/Container';
+
+function App() {
+	return (
+		<div className='container' align='center'>
+			<Header />
+			<Container />
+			<Footer />
+		</div>
+	);
+}
+
+export default App;
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+function Container() {
+	return <div>Main Container</div>;
+}
+
+export default Container;
